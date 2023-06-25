30821fe0e3d6c83216bcff3eb438af01e4da3177
190103291 <sarkytov87@gmail.com>
2023-06-25 16:47:48 +0600

    перенос на новый репо

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="wrapper clear">
+
+    <Drawer />
+    <Header />
+    <div className="content p-40">
+      <div className="d-flex align-center mb-40 justify-between">
+        <h1>All Sneakers</h1>
+        <div className="search-block d-flex">
+          <img src="/img/search.svg" alt="Search" />
+          <input placeholder="Search..." />
+        </div>
+      </div>
+
+      <div className="d-flex">
+        <Card />
+
+        <Card />
+
+        <Card />
+
+        <Card />
+      </div>
+
+
+
+
     </div>
-  );
+  </div >;
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+function Header() {
+    return (
+        <header className="d-flex justify-between align-center">
+            <div className="d-flex align-center">
+                <img width={40} height={40} src="/img/logo.png" alt="logo" />
+                <div>
+                    <h3 className="text-uppercase">React Sneakers</h3>
+                    <p className="opacity-5">Магазин лучших кроссовок</p>
+                </div>
+            </div>
+            <ul className="d-flex">
+                <li className="mr-30">
+                    <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+                    <span>1222 kzt.</span>
+                </li>
+                <li>
+                    <img width={20} height={20} src="/img/user.svg" alt="user" />
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
