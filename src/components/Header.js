91ba821b06386587317561ed9b0c209a9e4242f1
190103291 <sarkytov87@gmail.com>
2023-06-25 16:51:51 +0600

    testing branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,10 @@ function Header() {
                 <img width={40} height={40} src="/img/logo.png" alt="logo" />
                 <div>
                     <h3 className="text-uppercase">React Sneakers</h3>
-                    <p className="opacity-5">Магазин лучших кроссовок</p>
+                    <p className="opacity-5">ASDASDASDADADADAS</p>
                 </div>
             </div>
-            <ul className="d-flex">
-                <li className="mr-30">
-                    <img width={18} height={18} src="/img/cart.svg" alt="cart" />
-                    <span>1222 kzt.</span>
-                </li>
-                <li>
-                    <img width={20} height={20} src="/img/user.svg" alt="user" />
-                </li>
-            </ul>
+           
         </header>
     )
 }
