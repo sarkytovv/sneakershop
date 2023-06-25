b3cff250131f45a70eb909b3d0207f78182ff88d
190103291 <sarkytov87@gmail.com>
2023-06-25 17:00:03 +0600

    changing texts

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,17 @@ function Header() {
                 <img width={40} height={40} src="/img/logo.png" alt="logo" />
                 <div>
                     <h3 className="text-uppercase">React Sneakers</h3>
-                    <p className="opacity-5">ASDASDASDADADADAS</p>
+                    <p className="opacity-5">Магазин лучших кроссовок</p>
                 </div>
             </div>
             <ul className="d-flex">
                 <li className="mr-30">
                     <img width={18} height={18} src="/img/cart.svg" alt="cart" />
-                    <span>23232322 kzt.</span>
+                    <span>21212 kzt.</span>
                 </li>
-                {/* <li>
+                <li>
                     <img width={20} height={20} src="/img/user.svg" alt="user" />
-                </li> */}
+                </li>
             </ul>
         </header>
     )
