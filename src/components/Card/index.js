dfcf39ed0c98147cc2e751612e7ebf1070555601
190103291 <sarkytov87@gmail.com>
2023-07-04 02:39:27 +0600

    move data to MockAPI and take it from `backend`, fetch data (using useEffect hook for only one requst on page load, add items to cart)

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Card.module.scss'
 
-function Card(props) {
+function Card({ title, price, imageUrl, onFavorite, onPlus }) {
     const [isAdded, setIsAdded] = React.useState(false);
 
     const onClickPlus = () => {
+        onPlus({ title, price, imageUrl });
         setIsAdded(!isAdded);
     }
 
@@ -14,15 +15,15 @@ function Card(props) {
 
     return (
         <div className={styles.card}>
-            <div className="favorite" onClick={props.onFavorite}>
+            <div className="favorite" onClick={onFavorite}>
                 <img src="/img/unliked.svg" alt="not added to fav" />
             </div>
-            <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-            <h5>{props.title}</h5>
+            <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+            <h5>{title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>{props.price} kzt.</b>
+                    <b>{price} kzt.</b>
                 </div>
 
                 <img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,42 +1,25 @@
-function Drawer(props) {
+function Drawer({ onClose, items = [] }) {
     return (
         <div className="overlay">
             <div className="drawer">
-                <h2 className="d-flex justify-between mb-30">Корзина <img onClick={props.onClose} className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Close"></img></h2>
+                <h2 className="d-flex justify-between mb-30">
+                    Корзина
+                    <img onClick={onClose} className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Close"></img></h2>
 
                 <div className="items" style={{ flex: 1 }}>
-                    <div className="cartItem d-flex align-center mb-20">
+                    {
+                        items.map(obj =>
+                            <div className="cartItem d-flex align-center mb-20">
 
-                        <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+                                <div style={{ backgroundImage: `url(${obj.imageUrl})` }} className="cartItemImg"></div>
 
-                        <div className="mr-20 flex ">
-                            <p className="mb-5">LLDSALDLASDLASLDLAS L LADSL LASL DALS DLAS LDSA</p>
-                            <b>12 999kzt.</b>
-                        </div>
-                        <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"></img>
-                    </div>
-
-                    <div className="cartItem d-flex align-center mb-20">
-
-                        <div style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }} className="cartItemImg"></div>
-
-                        <div className="mr-20 flex">
-                            <p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
-                            <b>12 999kzt.</b>
-                        </div>
-                        <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"></img>
-                    </div>
-
-                    <div className="cartItem d-flex align-center">
-
-                        <div style={{ backgroundImage: 'url(/img/sneakers/3.jpg)' }} className="cartItemImg"></div>
-
-                        <div className="mr-20 flex">
-                            <p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
-                            <b>12 999kzt.</b>
-                        </div>
-                        <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"></img>
-                    </div>
+                                <div className="mr-20 flex ">
+                                    <p className="mb-5">{obj.title}</p>
+                                    <b>{obj.price}</b>
+                                </div>
+                                <img className="removeBtn" src={"/img/btn-remove.svg"} alt="Remove"></img>
+                            </div>)
+                    }
 
                 </div>
 
